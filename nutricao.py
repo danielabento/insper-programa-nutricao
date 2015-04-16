f175c2c5e490a696e4e22c7c47dd800e1783e6d4
fernandadg <fefsgabriel@hotmail.com>
2015-04-16 18:20:58 -0300

    continuacao 1- fe
    formula das calorias e dicionario das calorias

--- nutricao.py
+++ nutricao.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Apr 16 17:16:50 2015
+Created on Tue Apr 14 11:10:51 2015
 
-@author: Dani Bento
+@author: david
 """
 
 arquivoa = open("alimentos.csv", encoding="latin1")
@@ -16,3 +16,26 @@ print("")
 linha1 = usuario[1]
 dados = linha1.split(",")
 print(dados)
+print("")
+
+peso = int(dados[2])
+altura = float(dados[4])*100
+idade = int(dados[1])
+
+if dados[3]=="M":
+    formulah = 88.36 + (13.4 * peso) + (4.8 * altura) - (5.7 * idade)
+    print("A quantidade diaria de calorias ideal para seu corpo eh:", formulah)
+    print("")
+if dados[3]=="F":
+    formulam = 447.6 + (9.2 * peso) + (3.1 * altura) - (4.3 * idade)
+    print("A quantidade diaria de calorias ideal para seu corpo eh:", formulam)
+    print("")
+
+dic_cal = {}
+for linha in alimentos:
+    comidas = linha.split(",")
+    chave = comidas[0]
+    calorias = comidas[2]
+    dic_cal[chave] = calorias
+print(dic_cal)
+    
